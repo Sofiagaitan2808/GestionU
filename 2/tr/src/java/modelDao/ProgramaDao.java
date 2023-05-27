f60efb1f67a11e938d3e183fe0311243fc2f22d5
Sofiagaitan2808 <113397745+Sofiagaitan2808@users.noreply.github.com>
2023-05-26 20:47:48 -0500

    Add files via upload

---
+++ 2/tr/src/java/modelDao/ProgramaDao.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModelDao;
+
+
+import config.Conexion;
+import interfaz.MePrograma;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.swing.JOptionPane;
+import model.Programa;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+public class ProgramaDao implements MePrograma{
+
+    Conexion cn = new Conexion();
+    Programa po = new Programa();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+        
+    @Override
+    public Programa list(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List listadopro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean registrarpro(Programa pro) {
+        String sql="insert into programa(codprograma,nombrepro)values('"+pro.getCodprograma()+"','"+pro.getNomprograma()+"')";
+        try{
+            con=cn.getConnection();
+            ps=con.prepareStatement(sql);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null,"programa registrado");
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"programa no registrado");
+        }
+        
+        return false;
+        
+        
+    }
+
+    @Override
+    public boolean actualizarpro(Programa pro) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminarpro(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
